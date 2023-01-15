484a634a75232498950ef2f0abadfd2585cd0258
moeeahmed <m.ahamedbusiness@gmail.com>
2023-01-15 22:53:09 +0000

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,34 @@
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".navbar");
+    $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+  });
+
+  // MENU
+  $(".navbar-collapse a").on("click", function () {
+    $(".navbar-collapse").collapse("hide");
+  });
+
+  // AOS ANIMATION
+  AOS.init({
+    disable: "mobile",
+    duration: 800,
+    anchorPlacement: "center-bottom",
+  });
+
+  // SMOOTHSCROLL NAVBAR
+  $(function () {
+    $(".navbar a, .hero-text a").on("click", function (event) {
+      var $anchor = $(this);
+      $("html, body")
+        .stop()
+        .animate(
+          {
+            scrollTop: $($anchor.attr("href")).offset().top - 49,
+          },
+          1000
+        );
+      event.preventDefault();
+    });
+  });
+});
